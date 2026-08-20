ebc5486954b79daae360937cf44591f54f5607f9
atablebzh-ship-it <atable.bzh@gmail.com>
2026-08-20 15:47:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1751,7 +1751,9 @@ if (p) {
 
       {renderImportModal()}
       {renderHiddenFileInput()}
-      <SeoContent />
+     <div style={{ position: "absolute", width: 1, height: 1, padding: 0, margin: -1, overflow: "hidden", clip: "rect(0, 0, 0, 0)", whiteSpace: "nowrap", border: 0 }}>
+  <SeoContent />
+</div>
     </div>
   );
 }
